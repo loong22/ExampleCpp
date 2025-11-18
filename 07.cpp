953e6b21bb6acd5e5088edfd0ee1f4484baf615d
loong22 <8003660@gmail.com>
2025-11-18 16:45:00 +0800

    添加文件: 取近似值

---
+++ 07.cpp
@@ -0,0 +1,23 @@
+// 07.cpp 取近似值
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ float x;
+ cin >> x;
+ cout << (int)(x + 0.5) << endl;
+    return 0;
+}
+
+/*
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number;
+    cin >> number;
+    cout << round(number) << endl;
+}
+*/
